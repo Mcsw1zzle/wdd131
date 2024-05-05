443457de15158f49c66e29aca64c4c97c509015b
Mcsw1zzle <parkermcswizzle@outlook.com>
2024-05-04 22:21:44 -0700

    fixed the issue

--- mission/mission.js
+++ mission/mission.js
@@ -1,16 +1,16 @@
-const themeSelector = document.getElementsByClassName('selector');
+const themeSelector = document.getElementById('selector');
 function changeTheme() {
     let selectedTheme = themeSelector.value;
     if (selectedTheme == 'dark') {
         let element = document.getElementById('bod');
         element.classList.add('dark');
-        let img = document.getElementsByClassName('image');
+        let img = document.getElementById('image');
         img.setAttribute('src', 'byui-logo_white.png');
     }
     else if (selectedTheme == 'light') {
         let element = document.getElementById('bod');
         element.classList.remove('dark');
-        let img = document.getElementsByClassName('image');
+        let img = document.getElementById('image');
         img.setAttribute('src', 'logo.webp');
     }
 }
