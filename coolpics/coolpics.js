ec41d206dd583a87f05f7f51fdca2b2463976685
Mcsw1zzle <parkermcswizzle@outlook.com>
2024-05-19 13:27:03 -0700

    I finally did it.

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -21,6 +21,23 @@ function handleResize() {
 function viewerTemplate(pic, alt) {
     return `<div class="viewer">
       <button class="close-viewer">X</button>
-      <img src="${pic}" alt="${alt}">
+      <img id="bigboy" src="${pic}" alt="${alt}">
       </div>`;
 }
+
+function viewHandler(event) {
+  let thingClicked = event.target;
+  let src = thingClicked.getAttribute("src");
+  let srcParts = src.split("-");
+  let newSrc = srcParts[0] + "-full.jpeg";
+  document.body.insertAdjacentHTML("afterbegin", viewerTemplate(newSrc, "large image."));
+  function closeViewer() {
+    let removeableDiv = document.querySelector(".viewer");
+    removeableDiv.remove();
+  }
+  const x = document.querySelector(".close-viewer");
+  x.addEventListener("click", closeViewer);
+}
+let imageClicked = document.querySelector(".gallery");
+imageClicked.addEventListener("click", viewHandler);
+
